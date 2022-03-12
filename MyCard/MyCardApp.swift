f41b6427f44e9731dbc00cadceb84b97bb45b1de
박힘찬 <himchan05@naver.com>
2022-03-12 12:46:26 +0900

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by 박힘찬 on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
